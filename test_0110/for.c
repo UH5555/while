6daa2c6b44be6bb780fac1a3fec6797eeb2d570c
UH5555 <zhuhao162@163.com>
2022-01-12 20:47:14 +0800

    for.c再更新

--- test_0110/for.c
+++ test_0110/for.c
@@ -2,24 +2,131 @@
 
 #include <stdio.h>
 
+//寻找数组中的某一个数
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 17;
+//
+//	int sz = sizeof(arr) / sizeof(arr[0]);//计算元素个数
+//	int left = 0;//左下标
+//	int right = sz - 1;//右下标
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else
+//		{
+//			printf("founded, No.%d\n", mid);
+//			break;
+//		}
+//	}
+//	if (left > right)
+//	{
+//		printf("Not found");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[] = {1,2,3,4,5,6,7,8,9,10 };
+//	int k = 17;
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (k == arr[i])
+//		{
+//			printf("找到了,下标是:%d", i);
+//			break;
+//		}
+//	}
+//	if (i == sz)
+//		printf("找不到");
+//	return 0;
+//}
+
+//10的阶乘一直加到1的阶乘-----我写的
 //int main()
 //{
 //	int i = 1;
 //	int j = 1;
 //	int s = 0;
+//	int n = 10;
+//	int m = 1;
 //	for (i = 1; i <= 10; i++)
 //	{
-//		j = 
-//			for (j = 1; j <= 10; j++)
+//		int m = 1;
+//			for (j = 1; j <= n; j++)
 //			{
-//
+//				m = m * j;
 //			}
-//		s = s+j
+//			n--;
+//			s = s + m;
 //	}
+//	printf("%d", s);
 //	return 0;
 //}
 
-//n的阶乘
+//10的阶乘一直加到1的阶乘-----老师写的(再优化）
+//int main()
+//{
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		ret = ret * n;
+//		sum = sum + ret;
+//	}
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+//10的阶乘一直加到1的阶乘-----老师写的
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		ret = 1;
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret = ret * i;
+//		}
+//		sum = sum + ret;
+//	}
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+//n的阶乘----老师写的
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret * i;
+//	}
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
+
+//n的阶乘---自己写的
 //int main()
 //{
 //	int s = 1;
